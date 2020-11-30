00e232073d7e10bcce6cc45b33cc1a6c33875484
Douglas Ramos <douglas.dcr@outlook.com>
2020-11-29 21:36:06 -0300

    application page almost finished

--- src/application-page/application-page.component.tsx
+++ src/application-page/application-page.component.tsx
@@ -1,80 +1,141 @@
-import { Typography } from '@material-ui/core';
-// import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+import {
+  Card,
+  Checkbox,
+  CheckboxProps,
+  Container,
+  FormControlLabel,
+  Typography,
+} from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
+import Chessboard from 'chessboardjsx';
+import React, { useState } from 'react';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   sectionTitle: {
-//     flexGrow: 1,
-//     lineHeight: '90px',
-//   },
-//   sectionContent: {
-//     textAlign: 'center',
-//     fontSize: '24px',
-//     lineHeight: '36px',
-//   },
-//   sectionLinks: {
-//     backgroundColor: ' #edebe9',
-//   },
-//   authorsSection: {},
-//   main: {
-//     alignItems: 'center',
-//   },
-//   mainContent: {
-//     flexBasis: 'unset',
-//     textAlign: 'left',
-//   },
-//   mainSection: {
-//     backgroundImage: 'linear-gradient(#fff, #edebe9)',
-//     height: '500px',
-//   },
-//   ctaButton: {
-//     width: '250px',
-//     height: '50px',
-//     justifyContent: 'left',
-//     borderRadius: '16px',
-//     fontSize: '20px',
-//     textTransform: 'capitalize',
-//   },
-//   paragraph: {
-//     fontSize: '24px',
-//     lineHeight: '32px',
-//   },
-//   aboutSection: {
-//     height: '450px',
-//     alignItems: 'center',
-//     display: 'flex',
-//   },
+import { samplePgn } from './chessboard-with-list/sample-pgn';
 
-//   imageButton: {
-//     position: 'absolute',
-//     right: '-100px',
-//     width: '200px',
-//   },
-// }));
+const GreenCheckbox = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})((props: CheckboxProps) => <Checkbox color="default" {...props} />);
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      display: 'flex',
+      justifyContent: 'center',
+      paddingTop: 30,
+      paddingBottom: 200,
+    },
+    cardChessBoard: {
+      boxShadow: '0 5px 30px rgba(0,0,0,.2);',
+    },
+    cardChessBoardBottom: {
+      display: 'flex',
+    },
+    cardChessBoardTop: {
+      padding: 16,
+      textAlign: 'left',
+    },
+    bestMoment: {
+      color: 'green',
+      // background: 'green',
+      fontWeight: 'bold',
+    },
+    listText: {
+      fontFamily: 'monospace',
+      '& span': {
+        fontSize: 20,
+      },
+    },
+    comment: {
+      width: 770,
+      textAlign: 'left',
+      margin: 15,
+    },
+  }),
+);
 
 export const ApplicationPage = (): JSX.Element => {
-  // const classes = useStyles();
+  const classes = useStyles();
 
-  // const buttonTheme = createMuiTheme({
-  //   palette: {
-  //     primary: {
-  //       main: '#b58863',
-  //       light: '#b58863',
-  //       dark: '#946f51',
-  //       contrastText: '#fff',
-  //     },
-  //   },
-  // });
+  const allPositions = samplePgn;
+
+  const [currentBoardState, setCurrentBoardState] = useState(allPositions[0]);
+  const [isOnlyBestMomentsCheck, setIsOnlyBestMomentsCheck] = useState(false);
+
+  const positions = isOnlyBestMomentsCheck
+    ? allPositions.filter((pos) => pos.isBestMoment)
+    : allPositions;
+
+  function renderRow(props: ListChildComponentProps) {
+    const { index, style } = props;
+
+    return (
+      <ListItem
+        button
+        style={style}
+        key={index}
+        onClick={() => setCurrentBoardState(positions[index])}
+        selected={index === currentBoardState.id}
+        className={positions[index].isBestMoment && classes.bestMoment}
+      >
+        <ListItemText
+          className={classes.listText}
+          primary={
+            <pre>
+              {`${`0${positions[index].id + 1}`.slice(-2)} -`} {positions[index].moveFrom} →{' '}
+              {`${positions[index].moveTo} `}
+              {positions[index].isBestMoment && '✅'}
+            </pre>
+          }
+        />
+      </ListItem>
+    );
+  }
 
   return (
-    <div>
-      <Typography>Hello World</Typography>
-    </div>
+    <>
+      <Container>
+        <Typography variant="h4">
+          Partida Exemplo - Evento Southern Chess Summer Invitational 2006)
+        </Typography>
+      </Container>
+      <Container className={classes.root}>
+        <Card variant="outlined" className={classes.cardChessBoard}>
+          <div className={classes.cardChessBoardTop}>
+            <FormControlLabel
+              control={
+                <GreenCheckbox
+                  checked={isOnlyBestMomentsCheck}
+                  onChange={(_, value) => {
+                    setIsOnlyBestMomentsCheck(value);
+                  }}
+                  name="checkedG"
+                />
+              }
+              label="Apenas melhores momentos"
+            />
+          </div>
+          <div className={classes.cardChessBoardBottom}>
+            <FixedSizeList height={600} width={200} itemSize={46} itemCount={positions.length}>
+              {renderRow}
+            </FixedSizeList>
+            <Chessboard position={currentBoardState.fen} width={600} />
+          </div>
+          <div className={classes.comment}>
+            <Typography>{`Comentário: ${currentBoardState.comment}`}</Typography>
+          </div>
+        </Card>
+      </Container>
+    </>
   );
 };
---
+++ src/application-page/chessboard-with-list/sample-pgn.ts
@@ -0,0 +1,51 @@
+export const samplePgn = [
+  {
+    id: 0,
+    moveFrom: 'd2',
+    moveTo: 'd4',
+    fen: 'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR',
+    isBestMoment: true,
+    comment: 'For those that know me, I play 1. d4 almost exclusively.',
+  },
+  {
+    id: 1,
+    moveFrom: 'e7',
+    moveTo: 'e6',
+    fen: 'rnbqkbnr/pppp1ppp/4p3/8/3P4/8/PPP1PPPP/RNBQKBNR',
+    isBestMoment: true,
+    comment:
+      "It is responses like this that drive me crazy. I'm pretty solid in most variations of the QGD and QGA, and black's response just took away my comfort zone.",
+  },
+  {
+    id: 2,
+    moveFrom: 'e2',
+    moveTo: 'e4',
+    fen: 'rnbqkbnr/pppp1ppp/4p3/8/3PP3/8/PPP2PPP/RNBQKBNR',
+    isBestMoment: false,
+    comment: "If he's giving, I'm taking.....",
+  },
+  {
+    id: 3,
+    moveFrom: 'g8',
+    moveTo: 'f6',
+    fen: 'rnbqkb1r/pppp1ppp/4pn2/8/3PP3/8/PPP2PPP/RNBQKBNR',
+    isBestMoment: true,
+    comment: '',
+  },
+  {
+    id: 4,
+    moveFrom: 'c1',
+    moveTo: 'g5',
+    fen: 'rnbqkb1r/pppp1ppp/4pn2/6B1/3PP3/8/PPP2PPP/RN1QKBNR',
+    isBestMoment: true,
+    comment: 'I had numerous alternatives here, but decided to start the tactics early.',
+  },
+  {
+    id: 5,
+    moveFrom: 'h7',
+    moveTo: 'h6',
+    fen: 'rnbqkb1r/pppp1pp1/4pn1p/6B1/3PP3/8/PPP2PPP/RN1QKBNR',
+    isBestMoment: false,
+    comment: 'Attempting to drive the bishop back.',
+  },
+];
