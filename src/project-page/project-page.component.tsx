9e0d478352aaa43671f5fcdeda808abfcbb25006
Douglas Ramos <douglas.dcr@outlook.com>
2020-12-06 04:04:13 -0300

    main section background back

--- src/project-page/project-page.component.tsx
+++ src/project-page/project-page.component.tsx
@@ -15,6 +15,7 @@ const useStyles = makeStyles((theme) => ({
       paddingBottom: '100px',
       fontSize: '24px',
       lineHeight: '36px',
+      backgroundImage: 'linear-gradient(#fff, #edebe9)',
       [theme.breakpoints.down('sm')]: {
         paddingTop: '50px',
       },
